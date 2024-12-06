4589fe30697fafe0d6ed2d46bd864ba0010a6144
HEDO <hedo@oocl.com>
2024-12-06 15:59:43 +0800

    chore: init project

---
+++ src/main/java/com/oocl/ita/ColdPluseApplication.java
@@ -0,0 +1,11 @@
+package com.oocl.ita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdPluseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ColdPluseApplication.class, args);
+    }
+}
