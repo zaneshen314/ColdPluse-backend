0b111704ad81900955dbfaf4161bff8766a4c671
WONGTO6 <wongto6@oocl.com>
2024-12-09 10:07:55 +0800

    feat: fix entity

--- src/main/java/com/oocl/ita/web/domain/po/Venue.java
+++ src/main/java/com/oocl/ita/web/domain/po/Venue.java
@@ -13,16 +13,12 @@ public class Venue {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
     private String location;
 
-    @Column(nullable = false)
     private int capacity;
 
-    @Column(nullable = false)
     private String state;
 
 
