152386a233954ca05bfde9ca54a9cacd10012e5c
WONGTO6 <wongto6@oocl.com>
2024-12-09 18:00:01 +0800

    refactor: remove unused packages

--- src/main/java/com/oocl/ita/web/controller/UserController.java
+++ src/main/java/com/oocl/ita/web/controller/UserController.java
@@ -3,7 +3,6 @@ package com.oocl.ita.web.controller;
 import com.oocl.ita.web.domain.po.CharityEventParticipation;
 import com.oocl.ita.web.service.CharityEventService;
 import com.oocl.ita.web.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
