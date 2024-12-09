adaa6caf3f8d6061a335d82214ce39bc9e4b4efb
HEDO <hedo@oocl.com>
2024-12-09 14:10:37 +0800

    feat: add Ticket and Transaction entity

---
+++ src/main/java/com/oocl/ita/web/domain/po/Ticket.java
@@ -0,0 +1,26 @@
+package com.oocl.ita.web.domain.po;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer concertClassId;
+
+    private Integer concertScheduleId;
+
+    private Integer userId;
+
+    private String idCardNum;
+
+    private String state;
+
+    private Integer transactionId;
+}
