1e5617b9758dc234a69098b3f3476fd4f794e92c
SHENZA <shenza@oocl.com>
2024-12-11 01:32:11 +0800

    chore,adjust security config permit

--- src/main/java/com/oocl/ita/web/core/security/config/SecurityConfig.java
+++ src/main/java/com/oocl/ita/web/core/security/config/SecurityConfig.java
@@ -96,7 +96,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                         "/**/*.html",
                         "/**/*.css",
                         "/**/*.js",
-                        "/profile/**"
+                        "/profile/**",
+                        "/concerts/**",
+                        "/charity-events/**"
                 ).permitAll()
                 .antMatchers("/swagger-ui.html").anonymous()
                 .antMatchers("/swagger-resources/**").anonymous()
@@ -104,7 +106,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .antMatchers("/*/api-docs").anonymous()
                 .antMatchers("/druid/**").anonymous()
                 // 除上面外的所有请求全部需要鉴权认证
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .headers().frameOptions().disable();
         httpSecurity.logout().logoutUrl("/logout").logoutSuccessHandler(logoutSuccessHandler);
