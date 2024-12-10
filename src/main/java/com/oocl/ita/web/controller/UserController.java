81ab8932432c8456525824610471421d6d0672a2
WONGTO6 <wongto6@oocl.com>
2024-12-10 15:58:33 +0800

    [12] feat: finish getCharityEventIdsByUserId

--- src/main/java/com/oocl/ita/web/controller/UserController.java
+++ src/main/java/com/oocl/ita/web/controller/UserController.java
@@ -3,6 +3,7 @@ package com.oocl.ita.web.controller;
 import com.oocl.ita.web.domain.po.CharityEvent;
 import com.oocl.ita.web.domain.po.CharityEventParticipation;
 import com.oocl.ita.web.domain.vo.charity.UserCharityEventParticipationResp;
+import com.oocl.ita.web.repository.CharityEventParticipationRepository;
 import com.oocl.ita.web.service.CharityEventService;
 import com.oocl.ita.web.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +19,12 @@ public class UserController {
 
     private final UserService userService;
     private final CharityEventService charityEventService;
+    private final CharityEventParticipationRepository charityEventParticipationRepository;
 
-    public UserController(UserService userService, CharityEventService charityEventService) {
+    public UserController(UserService userService, CharityEventService charityEventService, CharityEventParticipationRepository charityEventParticipationRepository) {
         this.userService = userService;
         this.charityEventService = charityEventService;
+        this.charityEventParticipationRepository = charityEventParticipationRepository;
     }
 
     @GetMapping("/{userId}/cumulatedPoint")
@@ -39,6 +42,11 @@ public class UserController {
                     CharityEvent charityEvent = charityEventService.getById(charityEventParticipation.getCharityEventId());
                     return new UserCharityEventParticipationResp(charityEventParticipation, charityEvent);
                 }).toList();
+    }
+
+    @GetMapping("/{userId}/charity-events")
+    public List<Integer> getCharityEventIdsByUserId(@PathVariable Integer userId) {
+        return charityEventParticipationRepository.findAllByUserId(userId).stream().map(CharityEventParticipation::getCharityEventId).toList();
     }
 
 
