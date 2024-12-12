9c29216bc1f8b442010121fdb78517d5cc2cfca1
WONGTO6 <wongto6@oocl.com>
2024-12-12 17:51:06 +0800

    [20] fix: fix null error

--- src/main/java/com/oocl/ita/web/domain/bo/Concert/ConcertScheduleRegBody.java
+++ src/main/java/com/oocl/ita/web/domain/bo/Concert/ConcertScheduleRegBody.java
@@ -13,13 +13,20 @@ public class ConcertScheduleRegBody {
     public ConcertScheduleRegBody() {
     }
 
-    public ConcertScheduleRegBody(Integer concertId, String startTime, Long duration) {
+    public ConcertScheduleRegBody(Integer concertId, String startTime, Long duration, String saleStartTime) {
         this.concertId = concertId;
         this.startTime = startTime;
         this.duration = duration;
         this.saleStartTime = saleStartTime;
     }
 
+    public String getSaleStartTime() {
+        return saleStartTime;
+    }
+
+    public void setSaleStartTime(String saleStartTime) {
+        this.saleStartTime = saleStartTime;
+    }
 
     public Integer getConcertId() {
         return concertId;
